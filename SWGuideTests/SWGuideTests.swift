85fc4e42ccdd3ddae558b94a2992cbee1e1039a6
Darkkrye <pierre.boudon.27@gmail.com>
2019-07-07 16:29:50 +0200

    Initial Commit

---
+++ SWGuideTests/SWGuideTests.swift
@@ -0,0 +1,34 @@
+//
+//  SWGuideTests.swift
+//  SWGuideTests
+//
+//  Created by Pierre on 07/07/2019.
+//  Copyright © 2019 Pierre. All rights reserved.
+//
+
+import XCTest
+@testable import SWGuide
+
+class SWGuideTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
